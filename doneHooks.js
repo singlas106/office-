4f5ef0e942f3addc217c28b45a651944056b6612
singlas106 <100565945+singlas106@users.noreply.github.com>
2022-06-14 13:03:32 +0530

    Add files via upload

--- doneHooks.js
+++ doneHooks.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import table from "./tables";
 
 const App = () =>{
   const [state, setState] = useState({ firstName: '', lastName: '', fatherName: '', motherName: ''});
@@ -38,15 +39,33 @@ const App = () =>{
   return(
     <div className="pa5 bg-light-blue">
       <label>
-            firstName: <input type="text" name="text" placeholder="firstname" value={state.firstName} onChange={(e) => onInputChange("firstName",e.target.value)}/> <br/>
+          firstName: <input type="text" name="text" placeholder="firstname" value={state.firstName} onChange={(e) => onInputChange("firstName",e.target.value)}/> <br/>
           lastName: <input type="text" name="text" placeholder="lastname" value={state.lastName} onChange={(e) => onInputChange("lastName",e.target.value)}/> <br/>
           fatherName: <input type="text" name="text" placeholder="fathername" value={state.fatherName} onChange={(e) => onInputChange("fatherName",e.target.value)}/> <br/>
           motherName: <input type="text" name="text" placeholder="mothername" value={state.motherName} onChange={(e) => onInputChange("motherName",e.target.value)}/> <br/>
-          homeTown: <input type="text" name="text" placeholder="residence" /> <br/>
-          email: <input type="email" name="text" placeholder="Email" onChange={(e) => onInputChange("email",e.target.value)}/> <br/>
+          {/* homeTown: <input type="text" name="text" placeholder="residence" /> <br/> */}
+          {/* email: <input type="email" name="text" placeholder="Email" onChange={(e) => onInputChange("email",e.target.value)}/> <br/> */}
           <button onClick={onSupport}>submit</button>
         </label> 
+
+
+
+        <table>
+      <tr>
+        <th>firstName</th>
+        <th>lastName</th>
+        <th>fatherName</th>
+        <th>motherName</th>
+      </tr>{array.map(item => (
+        <tr>
+        <th>{item.firstName}</th>
+        <th>{item.lastName}</th>
+        <th>{item.fatherName}</th>
+        <th>{item.motherName}</th>
+      </tr>
+      ))}
+   </table> 
     </div>
   )
-}u
+}
 export default App;
